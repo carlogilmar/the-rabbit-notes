9e634bb86e6741861a49516286700a56a594dd42
Carlo Gilmar <carlogilmar12@gmail.com>
2025-06-29 23:02:12 -0600

    Update

--- dojo/SICP/29_ordered_sets.py
+++ dojo/SICP/29_ordered_sets.py
@@ -8,8 +8,18 @@ def is_element(ordered_set, element):
     else:
         return is_element(ordered_set[1:], element)
 
+def adjoin_set(set, element):
+    return set
+
 print("---Ordered Sets!-----")
 ordered_set = [1,3,4,5,6,7,8]
 print(f"is_element <> {is_element(ordered_set, 5)}")
 ordered_set = [1,3,4,5,6,7,8]
 print(f"is_element <> {is_element(ordered_set, 9)}")
+
+test = adjoin_set(3, [1, 2, 4, 5])     # → [1, 2, 3, 4, 5]
+print(f"Adjoin set: {test}")
+test = adjoin_set(4, [1, 2, 3, 4, 5])  # → [1, 2, 3, 4, 5]
+print(f"Adjoin set: {test}")
+test = adjoin_set(0, [1, 2, 3])        # → [0, 1, 2, 3]
+print(f"Adjoin set: {test}")
